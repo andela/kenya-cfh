9e3f657b98511bf86c6f83e5f53429a6ca7221d9
Rachel Abaniwo <rachel.abaniwo@gmail.com>
2018-01-19 16:25:59 +0100

    feature(gaming-screen):re-design gaming screen
    add bootstrap and jquery files to foot.jade to foot.jade file
    add bootstrap file to head.jade file
    add more user colors to directives.js file
    refactor game.js file
    add show-options method to index.js file
    increase player limit to 12 in game.js file
    add html files for gaming screens
    add css files for gaming screen
    
    [Finishes #153756411]

--- public/js/controllers/index.js
+++ public/js/controllers/index.js
@@ -1,10 +1,17 @@
-angular.module('mean.system')
-.controller('IndexController', ['$scope', 'Global', '$location', 'socket', 'game', 'AvatarService', function ($scope, Global, $location, socket, game, AvatarService) {
+angular.module("mean.system").controller("IndexController", [
+  "$scope",
+  "$window",
+  "$location",
+  "Global",
+  "socket",
+  "game",
+  ($scope, $window, $location, Global, socket, game) => {
     $scope.global = Global;
+    $scope.showOptions = true;
 
     $scope.playAsGuest = function() {
       game.joinGame();
-      $location.path('/app');
+      $location.path("/app");
     };
 
     $scope.showError = function() {
@@ -15,10 +22,21 @@ angular.module('mean.system')
       }
     };
 
-    $scope.avatars = [];
-    AvatarService.getAvatars()
-      .then(function(data) {
-        $scope.avatars = data;
-      });
+    $scope.showOptions = () => {
+      if (window.localStorage.token) {
+        $scope.showOptions = false;
+      } else {
+        $scope.showOptions = true;
+      }
+    };
+    $scope.showOptions();
 
-}]);
+    $scope.signOut = () => {
+      window.localStorage.removeItem("token");
+      $location.path("/");
+      window.location.reload();
+    };
+
+    $scope.avatars = [];
+  }
+]);
