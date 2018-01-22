9c933b64cf4323ca7593f75ad2051e2d2ba3803c
Sanni Taiwo <tywosanni@gmail.com>
2018-01-22 16:07:36 +0100

    chore(eslint): fix eslint errors

--- config/env/all.js
+++ config/env/all.js
@@ -2,7 +2,7 @@ import dotenv from 'dotenv';
 import path from 'path';
 
 dotenv.config();
-const rootPath = path.normalize(__dirname + '/../..');
+const rootPath = path.normalize(`${__dirname}/../..`);
 
 const testDb = process.env.MONGOHQ_TEST;
 const prodDB = process.env.MONGOHQ_URL;
@@ -11,6 +11,7 @@ const database = (process.env.NODE_ENV === 'test') ? testDb : prodDB;
 
 export default {
   root: rootPath,
+  expressKey: process.env.EXPRESS,
   port: process.env.PORT || 3000,
   db: database,
   secret: process.env.SECRET
--- test/game/game.js
+++ test/game/game.js
@@ -1,4 +1,3 @@
-const should = require('should');
 const io = require('socket.io-client');
 
 const socketURL = 'http://localhost:3001';
@@ -10,64 +9,67 @@ const options = {
 
 
 describe('Game Server', () => {
-  it('Should accept requests to joinGame', (done) => {
-    const client1 = io.connect(socketURL, options);
-    const disconnect = () => {
-      client1.disconnect();
-      done();
-    };
-    client1.on('connect', () => {
-      client1.emit('joinGame', {
-        userID: 'unauthenticated',
-        room: '',
-        createPrivate: false
+  it(
+    'Should accept requests to join game when joinGame request is sent',
+    (done) => {
+      const client = io.connect(socketURL, options);
+      const disconnect = () => {
+        client.disconnect();
+        done();
+      };
+      client.on('connect', () => {
+        client.emit('joinGame', {
+          userID: 'unauthenticated',
+          room: '',
+          createPrivate: false
+        });
+        setTimeout(disconnect, 200);
       });
-      setTimeout(disconnect, 200);
-    });
-  });
+    }
+  );
 
   it('Should send a game update upon receiving request to joinGame', (done) => {
-    const client1 = io.connect(socketURL, options);
+    const socket = io.connect(socketURL, options);
     const disconnect = () => {
-      client1.disconnect();
+      socket.disconnect();
       done();
     };
-    client1.on('connect', () => {
-      client1.emit('joinGame', {
+    socket.on('connect', () => {
+      socket.emit('joinGame', {
         userID: 'unauthenticated',
         room: '',
         createPrivate: false
       });
-      client1.on('gameUpdate', (data) => {
-        data.gameID.should.match(/\d+/);
+      socket.on('gameUpdate', (response) => {
+        response.gameID.should.match(/\d+/);
       });
       setTimeout(disconnect, 200);
     });
   });
 
   it('Should announce new user to all users', (done) => {
-    const client1 = io.connect(socketURL, options);
-    let client2;
+    const client = io.connect(socketURL, options);
+    let socket;
     const disconnect = () => {
-      client1.disconnect();
-      client2.disconnect();
+      client.disconnect();
+      socket.disconnect();
       done();
     };
-    client1.on('connect', () => {
-      client1.emit('joinGame', {
+    client.on('connect', () => {
+      client.emit('joinGame', {
         userID: 'unauthenticated',
         room: '',
         createPrivate: false
       });
-      client2 = io.connect(socketURL, options);
-      client2.on('connect', () => {
-        client2.emit('joinGame', {
+      socket = io.connect(socketURL, options);
+      socket.on('connect', () => {
+        socket.emit('joinGame', {
           userID: 'unauthenticated',
           room: '',
           createPrivate: false
         });
-        client1.on('notification', (data) => {
-          data.notification.should.match(/ has joined the game\!/);
+        client.on('notification', (response) => {
+          response.notification.should.match(/ has joined the game\!/);
         });
       });
       setTimeout(disconnect, 200);
@@ -77,31 +79,31 @@ describe('Game Server', () => {
   it(
     'Should start game when startGame event is sent with 3 players',
     (done) => {
-      let client2;
-      let client3;
+      let client;
+      let events;
 
-      const client1 = io.connect(socketURL, options);
+      const socket = io.connect(socketURL, options);
       const disconnect = () => {
-        client1.disconnect();
-        client2.disconnect();
-        client3.disconnect();
+        socket.disconnect();
+        client.disconnect();
+        events.disconnect();
         done();
       };
       const expectStartGame = () => {
-        client1.emit('startGame');
-        client1.on('gameUpdate', (data) => {
-          data.state.should.equal('czar pick card');
+        socket.emit('startGame');
+        socket.on('gameUpdate', (response) => {
+          response.state.should.equal('czar pick card');
         });
-        client2.on('gameUpdate', (data) => {
-          data.state.should.equal('czar pick card');
+        client.on('gameUpdate', (response) => {
+          response.state.should.equal('czar pick card');
         });
-        client3.on('gameUpdate', (data) => {
-          data.state.should.equal('czar pick card');
+        events.on('gameUpdate', (response) => {
+          response.state.should.equal('czar pick card');
         });
         setTimeout(disconnect, 200);
       };
-      client1.on('connect', () => {
-        client1.emit(
+      socket.on('connect', () => {
+        socket.emit(
           'joinGame',
           {
             userID: 'unauthenticated',
@@ -109,9 +111,9 @@ describe('Game Server', () => {
             createPrivate: false
           }
         );
-        client2 = io.connect(socketURL, options);
-        client2.on('connect', () => {
-          client2.emit(
+        client = io.connect(socketURL, options);
+        client.on('connect', () => {
+          client.emit(
             'joinGame',
             {
               userID: 'unauthenticated',
@@ -119,9 +121,9 @@ describe('Game Server', () => {
               createPrivate: false
             }
           );
-          client3 = io.connect(socketURL, options);
-          client3.on('connect', () => {
-            client3.emit(
+          events = io.connect(socketURL, options);
+          events.on('connect', () => {
+            events.emit(
               'joinGame',
               {
                 userID: 'unauthenticated',
